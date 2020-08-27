452e6469d9663bf1d7fea7011816967f93b9f0ec
syan91 <syan199165@gmail.com>
2020-08-27 12:45:17 +0300

    Initial commit

---
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -0,0 +1,65 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+    @Test
+    public void currentTemperatureInRangeIncr() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(15);
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(20);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(21, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void currentTemperatureInRangeDecr() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(15);
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(29);
+        conditioner.decreasedCurrentTemperature();
+        assertEquals(28, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void currentTemperatureOnLimitDecr() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(15);
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(30);
+        conditioner.decreasedCurrentTemperature();
+        assertEquals(29, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void currentTemperatureOnLimitIncr() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(15);
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(15);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(16, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void currentTemperatureOverLimitMin() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(15);
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(-10);
+        assertEquals(0, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void currentTemperatureOverLimitMax() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(15);
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(100);
+        assertEquals(0, conditioner.getCurrentTemperature());
+    }
+}
